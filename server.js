33a144ad7b2b6756a7457144859e046ac35605b6
cai5232 <caicai523261@qq.com>
2026-08-17 20:57:12 +0800

    Increase MCP request size for long code pushes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { SSEServerTransport } from '@modelcontextprotocol/sdk/server/sse.js';
 import { CallToolRequestSchema, ListToolsRequestSchema } from '@modelcontextprotocol/sdk/types.js';
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
